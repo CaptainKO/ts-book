abce70598dc7e05d22fca576e94849e619d94a90
CaptainKO <32269291+CaptainKO@users.noreply.github.com>
2018-09-14 16:39:49 +0700

    Update gulpfile.js + package.json + package-lock.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,50 @@
-// Declare
+// Declarartion
 var gulp = require('gulp');
 var tslint = require('gulp-tslint');
 var ts = require('gulp-typescript');
-
-gulp.task('default', function () {
+var browserify = require('browserify'),
+    transform = require('vinyl-transform'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps');
+var browserified = transform(function (filename) {
+    var b = browserify({
+        entries: filename,
+        debug: true
+    });
+    return b.bundle();
+});
+gulp.task('default', function () { // Declare a function named 'default'
     console.log('Hello World');
 });
 gulp.task('lint', function () {
     return gulp.src([
-        './source/ts/**/**.ts',
-        './test/**/**.ts'
-    ]).pipe(tslint())
-    .pipe(tslint.report('verbose'));
-})
+            './source/ts/**/**.ts', // Read All subfolders and all their *.ts files
+            './test/**/**.ts'
+        ]).pipe(tslint()) // Output stream will be sent to tslint()
+        .pipe(tslint.report('verbose')); // Output from tslint() will be sent to tslint.report function
+}); // Check whether our TypeScript code follows a series of recommended practices
 var tsProject = ts.createProject({
     removeComments: true,
-    noImplicitAny : true,
-    target : 'ES3',
-    module : 'commonjs',
-    declarationFiles : false
+    noImplicitAny: true,
+    target: 'ES3',
+    module: 'commonjs',
+    declarationFiles: false // All of these are options for ts complier (compiling ts to js).
 });
-gulp.task('tsc', function() {
-    return gulp.src('/source/ts/**/**.ts')
+gulp.task('tsc', function () {
+    return gulp.src('/source/ts/**/**.ts') // like lint task
         .pipe(ts(tsProject))
-        .js.pipe(gulp.dest('./temp/source/js'));
+        .js.pipe(gulp.dest('./temp/source/js')); // .js write it as *.js file and then write it down
 });
 var tsTestProject = ts.createProject({
-    removeComments : true,
-    noImplicitAny : true,
-    target : 'ES3',
-    module : 'commonjs',
-    declarationFiles : false
+    removeComments: true,
+    noImplicitAny: true,
+    target: 'ES3',
+    module: 'commonjs',
+    declarationFiles: false // Like tsProject
 });
-gulp.task('tsc-tests', function() {
+gulp.task('tsc-tests', function () {
     return gulp.src('.test/**/**.test.ts')
         .pipe(ts(tsTestProject))
         .js.pipe(gulp.dest('./temp/test/'));
 });
-gulp.task('default', ['lint', 'tsc', 'tsc-tests']);
+gulp.task('default', ['lint', 'tsc', 'tsc-tests']); // Add them as subtasks of default task
